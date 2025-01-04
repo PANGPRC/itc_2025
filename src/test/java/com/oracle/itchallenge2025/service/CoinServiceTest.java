6d650ce5183f0b2c10e01f6ae392a3cbf84484c0
PANGPRC <13808968158@139.com>
2025-01-04 12:12:02 +0800

    Modification for Unit Test

--- src/test/java/com/oracle/itchallenge2025/service/CoinServiceTest.java
+++ src/test/java/com/oracle/itchallenge2025/service/CoinServiceTest.java
@@ -18,9 +18,9 @@ public class CoinServiceTest {
 
     @Test
     public void testCalculateMinimumCoins() {
-        List<Double> coinDenominations = Arrays.asList(1.0, 0.5, 0.25, 0.1, 0.05);
+        List<Double> coinDenominations = Arrays.asList(0.01, 0.05, 0.1, 0.2, 0.5, 1.0);
         double targetAmount = 1.85;
-        List<Double> expected = Arrays.asList(1.0, 0.5, 0.25, 0.1);
+        List<Double> expected = Arrays.asList(0.05, 0.1, 0.2, 0.5, 1.0);
 
         List<Double> result = coinService.calculateMinimumCoins(targetAmount, coinDenominations);
 
